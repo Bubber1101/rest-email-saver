663cd2221028ab67656310e2ab9faf278dd969d1
Bubber1101 <patrykstradomski@wp.pl>
2020-10-10 16:30:31 +0200

    Added application.properties to be overridden with env file

--- src/main/java/open/mail/saver/EmailAddressLoggingService.java
+++ src/main/java/open/mail/saver/EmailAddressLoggingService.java
@@ -1,6 +1,7 @@
 package open.mail.saver;
 
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -8,9 +9,11 @@ import javax.enterprise.context.ApplicationScoped;
 @Slf4j
 public class EmailAddressLoggingService implements EmailAddressService {
 
+  @ConfigProperty(name = "log.message")
+  String message;
+
   @Override
   public void process(EmailAddress emailAddress) {
-
-    log.info("Persisting email address {}", emailAddress);
+    log.info("{}||Persisting email address {}", message, emailAddress);
   }
 }
